160ee8d64213ad0b0f8349749654b3581e4ef7c8
Annderson Rey <anndersonrey@gmail.com>
2023-05-24 11:59:25 +1000

    Creating Nav Bar

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,21 +1,35 @@
-import '../../App.css'
+import { useRoutes, BrowserRouter } from "react-router-dom";
 
-import { Home } from '../Home';
-import { MyAccount } from '../MyAccount';
-import { MyOrders } from '../MyOrders';
-import { MyOrder } from '../MyOrder';
-import { SignIn } from '../SignIn';
+import { Home } from "../Home";
+import { MyAccount } from "../MyAccount";
+import { MyOrders } from "../MyOrders";
+import { MyOrder } from "../MyOrder";
+import { SignIn } from "../SignIn";
+import { NotFound } from "../NotFound";
+import { Navbar } from "../../components/NavBar";
+
+import "./App.css";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
+
+  return routes;
+};
 
 function App() {
   return (
-    <>
-      <Home/>
-      <MyAccount/>
-      <MyOrder/>
-      <MyOrders/>
-      <SignIn/>
-    </>
-  )
+    <BrowserRouter>
+      <AppRoutes />
+      <Navbar />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
